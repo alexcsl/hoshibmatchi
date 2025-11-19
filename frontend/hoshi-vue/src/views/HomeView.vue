731ea9719c62c7c4b83814b1c78c834c158d9a4d
Alexander C.S.L <alexandercsl32@gmail.com>
2025-11-19 15:27:08 +0700

    REmoved unused files

--- frontend/hoshi-vue/src/views/HomeView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="home-page">
-    <h2>Home Feed</h2>
-    <div style="height: 2000px; background: #333;">
-      <p>Your feed content goes here...</p>
-    </div>
-  </div>
-</template>
